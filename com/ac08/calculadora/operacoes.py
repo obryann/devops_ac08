68b182e40b6b962e5fc44ff9912fd6172216850a
Vitmyself <vitor.duarte@aluno.faculdadeimpacta.com.br>
2019-10-24 20:52:55 -0300

    Adicionando arquivo de teste

--- com/ac08/calculadora/operacoes.py
+++ com/ac08/calculadora/operacoes.py
@@ -1,15 +1,18 @@
-def soma (num1, num2):
-	 a = num1 + num2 
-	 return a 
+def soma(num1, num2):
+    a = num1 + num2
+    return a
 
-def subt (num1, num2):
-	b = num1 - num2
-	return b
+
+def subt(num1, num2):
+    b = num1 - num2
+    return b
+
 
 def mult(num1, num2):
-	m = num1 * num2
-	return m
-	
+    m = num1 * num2
+    return m
+
+
 def divi(num1, num2):
-	d =	num1 / num2
-	return d
+    d = num1 / num2
+    return d
