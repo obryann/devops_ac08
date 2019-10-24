820afe1e24b1d759a7ddfb20f1d6938d029b7122
Guilherme Domingues Reinhardt <1901228@SALAS.AULAS>
2019-10-24 20:16:05 -0300

    ajustando

--- com/ac08/calculadora/app_web_start.py
+++ com/ac08/calculadora/app_web_start.py
@@ -3,8 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+
 def index():
-    return 'Index Page!'
+
+return 'Index Page!'
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
